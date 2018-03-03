708617f021aa26a759cf69356d21790528763c57
vladimirsaakian <31295797+vladimirsaakian@users.noreply.github.com>
2018-03-03 15:52:23 -0800

    “message”

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ZLR9vOGXXwRBvNXDyKwMgxtAB',
+  consumer_secret: 'tlXOyeccqnAuPU5bRnB8eKaiBMPo95x6YcpKJHddfaZgdXMl13',
+  access_token_key: '922238213778845696-5zcD0KF2GBqaWDTz4sDLcVlQcKRKoLb',
+  access_token_secret: 'goPP3u8Wx8D1dZfC1bYZu2fOAU2jfrWCNiaGikOk5j1Rs',
+}
+
+exports.spotifyKeys = {
+	id: '5cd1d172f98d42cd9296df65e920a800',
+	secret: '13d2cf2f85d04119bdebc354afceef57',
+}
